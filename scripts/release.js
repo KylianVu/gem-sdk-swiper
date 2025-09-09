25afd70310fc6f17f8625268e053d622f9742cfd
khanhxoan <khanhxoan99@gmail.com>
2025-09-10 02:46:52 +0700

    Fix release

--- scripts/release.js
+++ scripts/release.js
@@ -1,8 +1,6 @@
 import exec from 'exec-sh';
-import inquirer from 'inquirer';
 import fs from 'fs';
 import path from 'path';
-import { rimraf } from 'rimraf';
 import * as url from 'url';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url)));
@@ -18,62 +16,13 @@ async function release() {
   });
   const releaseDate = formatter.format(date);
 
-  const options = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'version',
-      message: 'Version:',
-      default: pkg.version,
-    },
-    {
-      type: 'list',
-      name: 'alpha',
-      message: 'Alpha?',
-      when: (opts) => opts.version.indexOf('alpha') >= 0,
-      choices: [
-        {
-          name: 'YES',
-          value: true,
-        },
-        {
-          name: 'NO',
-          value: false,
-        },
-      ],
-    },
-    {
-      type: 'list',
-      name: 'beta',
-      message: 'Beta?',
-      when: (opts) => opts.version.indexOf('beta') >= 0,
-      choices: [
-        {
-          name: 'YES',
-          value: true,
-        },
-        {
-          name: 'NO',
-          value: false,
-        },
-      ],
-    },
-    {
-      type: 'list',
-      name: 'next',
-      message: 'Next?',
-      when: (opts) => opts.version.indexOf('next') >= 0,
-      choices: [
-        {
-          name: 'YES',
-          value: true,
-        },
-        {
-          name: 'NO',
-          value: false,
-        },
-      ],
-    },
-  ]);
+  // Auto-release mode - no interactive prompts
+  const options = {
+    version: pkg.version,
+    alpha: false,
+    beta: false,
+    next: false,
+  };
   // Set version
   pkg.version = options.version;
   childPkg.version = options.version;
@@ -86,16 +35,6 @@ async function release() {
     `${JSON.stringify(childPkg, null, 2)}\n`,
   );
 
-  await exec.promise('git pull');
-  await exec.promise('npm i');
-  rimraf.sync(path.resolve(__dirname, 'dir'));
-  fs.mkdirSync(path.resolve(__dirname, 'dir'));
-  await exec.promise(`npm run build:prod`);
-  await exec.promise('git add .');
-  await exec.promise(`git commit -m "Release ${pkg.version}" --no-verify`);
-  await exec.promise('git push');
-  await exec.promise(`git tag v${pkg.version}`);
-  await exec.promise('git push origin --tags');
   if (options.beta) {
     await exec.promise('cd ./dist && npm publish --tag beta');
   } else if (options.alpha || options.next) {
